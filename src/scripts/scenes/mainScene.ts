8d3b8a3e4bea7901a3ba3dfa3065fdfedc8b8d98
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-05-04 12:51:25 +0300

    add loading text

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -9,8 +9,10 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
-    new PhaserLogo(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
+    this.cameras.main.fadeIn();
+
+    new PhaserLogo(this, this.cameras.main.width / 2, 0);
+    this.fpsText = new FpsText(this);
 
     // display the Phaser.VERSION
     this.add
@@ -18,10 +20,24 @@ export default class MainScene extends Phaser.Scene {
         color: '#000000',
         fontSize: '24px'
       })
-      .setOrigin(1, 0)
+      .setOrigin(1, 0);
+
+      // remove the loading screen
+    let loadingScreen = document.getElementById('loading-screen');
+    if (loadingScreen) {
+      loadingScreen.classList.add('transparent');
+      this.time.addEvent({
+        delay: 1000,
+        callback: () => {
+          // @ts-ignore
+          loadingScreen.remove();
+        }
+      });
+    }
+
   }
 
   update() {
-    this.fpsText.update()
+    this.fpsText.update();
   }
 }
