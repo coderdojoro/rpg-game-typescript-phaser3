4bb23fedccf7a1d4a5282e843c6b39ba423d0e9f
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-08-30 08:02:45 +0300

    fix sh

--- run-serve.sh
+++ run-serve.sh
@@ -1,4 +1,15 @@
 # build-server.sh
 #!/bin/bash
-set PORT=${PORT:-'8000'} 
+echo initial PORT: $PORT
+
+if [ -z "$PORT" ]
+then
+      export PORT=8000
+else
+      echo "keep PORT"
+fi
+
+echo "new PORT is $PORT"
+
 npx serve -l $PORT dist
+echo port: $PORT
